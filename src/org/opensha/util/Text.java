699f379d3684acabcc9532e788bf61a6d5c404a0
Peter Powers <pmpowers@usgs.gov>
2018-08-30 12:07:27 -0600

    whitespace splitter; no need for Parsing, yet

--- src/org/opensha/util/Text.java
+++ src/org/opensha/util/Text.java
@@ -2,6 +2,8 @@ package org.opensha.util;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Splitter;
 import com.google.common.base.StandardSystemProperty;
 import com.google.common.base.Strings;
 
@@ -19,6 +21,21 @@ public class Text {
   public static final String NULL = "null";
 
   /**
+   * Whitespace {@link Splitter splitter} that omits empty strings and trims the
+   * result. Shortcut for:
+   * 
+   * <pre>
+   * Splitter.on(CharMatcher.whitespace())
+   *     .omitEmptyStrings()
+   *     .trimResults();
+   * </pre>
+   */
+  public static final Splitter WHITESPACE_SPLITTER =
+      Splitter.on(CharMatcher.whitespace())
+          .omitEmptyStrings()
+          .trimResults();
+
+  /**
    * Verifies that the supplied {@code String} is neither {@code null} or empty.
    * Method returns the supplied value and can be used inline.
    *
@@ -26,7 +43,9 @@ public class Text {
    * @throws IllegalArgumentException if name is {@code null} or empty
    */
   public static String validateName(String name) {
-    checkArgument(!Strings.nullToEmpty(name).trim().isEmpty(), "Name may not be empty or null");
+    checkArgument(
+        !Strings.nullToEmpty(name).trim().isEmpty(),
+        "Name may not be empty or null");
     return name;
   }
 
