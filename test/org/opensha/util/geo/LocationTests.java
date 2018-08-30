78f1e9f8799b312ebcab6b38544b853f2a3a27cf
Peter Powers <pmpowers@usgs.gov>
2018-08-30 10:15:57 -0600

    loc tests: lon,lat --> lat,lon args

--- test/org/opensha/util/geo/LocationTests.java
+++ test/org/opensha/util/geo/LocationTests.java
@@ -5,15 +5,15 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.opensha.util.Maths;
 
 import com.google.common.base.Converter;
 import com.google.common.collect.Lists;
-
-import java.util.Collections;
-import java.util.List;
 
 @SuppressWarnings("javadoc")
 public class LocationTests {
@@ -28,7 +28,11 @@ public class LocationTests {
 
   @Test
   public final void create() {
-    Location loc = Location.create(V, V, V);
+    Location loc = new Location(V, V);
+    assertEquals(loc.longitude, V, 0);
+    assertEquals(loc.latitude, V, 0);
+    assertEquals(loc.depth, 0, 0);
+    loc = Location.create(V, V, V);
     assertEquals(loc.longitude, V, 0);
     assertEquals(loc.latitude, V, 0);
     assertEquals(loc.depth, V, 0);
@@ -40,22 +44,22 @@ public class LocationTests {
 
   @Test(expected = IllegalArgumentException.class)
   public final void create_IAE1() {
-    Location.create(0.0, 90.1);
+    Location.create(90.1, 0.0);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public final void create_IAE2() {
-    Location.create(0.0, -90.1);
+    Location.create(-90.1, 0.0);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public final void create_IAE3() {
-    Location.create(360.0, 0.0);
+    Location.create(0.0, 360.0);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public final void create_IAE4() {
-    Location.create(-360.0, 0.0);
+    Location.create(0.0, -360.0);
   }
 
   @Test
@@ -78,7 +82,7 @@ public class LocationTests {
 
   @Test
   public final void toStringTest() {
-    Location loc = Location.create(20, 30, 10);
+    Location loc = Location.create(30, 20, 10);
     String s = String.format("%.5f,%.5f,%.5f", loc.longitude, loc.latitude, loc.depth);
     assertEquals(loc.toString(), s);
   }
@@ -99,9 +103,9 @@ public class LocationTests {
     Location loc = Location.create(V, V, V);
     assertEquals(location, loc);
     // different values
-    loc = Location.create(V + 0.1, V, V);
-    assertNotEquals(location, loc);
     loc = Location.create(V, V + 0.1, V);
+    assertNotEquals(location, loc);
+    loc = Location.create(V + 0.1, V, V);
     assertNotEquals(location, loc);
     loc = Location.create(V, V, V + 0.1);
     assertNotEquals(location, loc);
@@ -111,19 +115,19 @@ public class LocationTests {
   public final void hashCodeTest() {
     Location other = Location.create(V, V, V);
     assertEquals(location.hashCode(), other.hashCode());
-    Location locA = Location.create(45, 90, 25);
-    Location locB = Location.create(90, 45, 25);
+    Location locA = Location.create(90, 45, 25);
+    Location locB = Location.create(45, 90, 25);
     assertNotEquals(locA.hashCode(), locB.hashCode());
   }
 
   @Test
   public final void compareToTest() {
-    Location l0 = Location.create(-30, 20);
-    Location l1 = Location.create(-50, 20);
-    Location l2 = Location.create(10, -10);
-    Location l3 = Location.create(30, -10);
-    Location l4 = Location.create(30, -10);
-    Location l5 = Location.create(10, 40);
+    Location l0 = Location.create(20, -30);
+    Location l1 = Location.create(20, -50);
+    Location l2 = Location.create(-10, 10);
+    Location l3 = Location.create(-10, 30);
+    Location l4 = Location.create(-10, 30);
+    Location l5 = Location.create(40, 10);
     List<Location> locList = Lists.newArrayList(l0, l1, l2, l3, l4, l5);
     Collections.sort(locList);
     assertTrue(locList.get(0) == l2);
